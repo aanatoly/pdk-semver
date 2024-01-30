851444ef75e7cf6c99cbfa0f414e60816b7de08e
Jon Doe <jon@example.com>
2024-01-30 17:55:56 +0200

    add git version support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,12 +2,20 @@
 
 ## [Unreleased]
 
+### Added
+ * git support
+
+## [0.1.0] - 2024-01-27
+
+### Added
+ * initial commit
+
 [Unreleased]: https://github.com/aanatoly/pdk-semver
 [0.1.0]: https://github.com/aanatoly/pdk-semver/releases/tag/0.1.0
 
 ------
-The format is based on [Keep a Changelog][kacl] and [Common Changelog][ccl] styles
-and this project adheres to [Semantic Versioning][semver]
+Changelog format is based on [Keep a Changelog][kacl] and [Common Changelog][ccl] styles.<br>
+Version format adheres to [Semantic Versioning][semver].
 
 [semver]: https://semver.org/spec/v2.0.0.html "Semantic Versioning"
 [kacl]: https://keepachangelog.com/en/ "Keep a Changelog"
--- src/pdk_semver/__main__.py
+++ src/pdk_semver/__main__.py
@@ -1,38 +1,113 @@
 """
-Demonstrates pydevkit features.
+Pretty print package version.
+
+The full version style has these components:
+[base]-[revision].[release info].[build info]
+
+You can override components via command line options.
 
 EPILOG:
-Example 1:
-```
-PYDEVKIT_LOG_HANDLER=app ./script --log-level=debug
-```
+Examples:
+Print version in default style, e.g. internal
+  $ pdk-semver
+  3.2.2-rev.3.git.b674019
 
-Example 2:
-```
-./script --log-level=debug --log-handler=json
-```
+Print version in default style, with release info "arch.aarch64"
+  $ pdk-semver --rinfo-value arch.aarch64
+  3.2.2-rev.3.arch.aarch64.git.b674019
+
+Print all styles
+  $ pdk-semver -s all --rinfo-value some.info
+  internal 3.2.2-rev.3.some.info.git.b674019
+    public 3.2.2-rev.3.some.info
+   baserev 3.2.2-rev.3
+      base 3.2.2
+    latest 3.2
 """
 
 
 import logging
 import sys
-import threading
 
 import pydevkit.log.config  # noqa: F401
 from pydevkit.argparse import ArgumentParser
-from pydevkit.conf import conf_get
-from pydevkit.log import prettify
 from pydevkit.shell import Shell
-from pydevkit.term import term_get
 
 from . import __version__
+from .ver import Version
 
 log = logging.getLogger(__name__)
 
 
+class GitVals:
+    @staticmethod
+    def supported(path):
+        sh = Shell()
+        sh["dir"] = path
+        sh["git"] = 'git -C "%(dir)s"' % sh
+        sh("%(git)s rev-parse --git-dir >& /dev/null")
+
+    @staticmethod
+    def get_vals(adir, aref) -> dict:
+        sh = Shell()
+        sh["dir"] = adir
+        sh["ref"] = aref
+        sh["git"] = 'git -C "%(dir)s"' % sh
+        rc = {}
+        # find closest tag
+        val = sh.inp("%(git)s describe --abbrev=0 %(ref)s 2>/dev/null | cat")
+        rc["base_value"] = val if val else "0.1.0"
+        if val:
+            val += ".."
+        sh["tag"] = val
+        val = int(sh.inp("%(git)s rev-list %(tag)s%(ref)s --count"))
+        rc["rev_value"] = val
+        val = sh.inp("%(git)s rev-parse --short %(ref)s")
+        rc["binfo_value"] = ["git", val]
+        return rc
+
+
+def get_vc_vals(path, ref) -> dict:
+    for cls in [GitVals]:
+        try:
+            cls.supported(path)
+            log.debug("git repo found at '%s", path)
+            return cls.get_vals(path, ref)
+        except Exception:
+            pass
+    log.warning("no version control found at '%s", path)
+    return {}
+
+
+def get_cmd_vals(args) -> dict:
+    vals = {}
+    for k, v in vars(args).items():
+        pfx = k.split("_", 1)[0]
+        if pfx in ["rinfo"]:
+            vals[k] = v
+    return vals
+
+
 def get_args():
-    p = ArgumentParser(help=__doc__, version=__version__)
-    p.add_argument("--file", help="file to check")
+    p = ArgumentParser(help=__doc__, version=__version__, usage="short")
+    p.add_argument(
+        "-C", help="path to git repo", dest="path", metavar="path", default="."
+    )
+    p.add_argument(
+        "-r", help="git revision ref", dest="ref", metavar="ref", default="HEAD"
+    )
+    styles = [*list(Version.Styles.keys()), "all"]
+    p.add_argument(
+        "-s",
+        help="style, one of %(choices)s",
+        dest="style",
+        metavar="name",
+        default="internal",
+        choices=styles,
+    )
+    p.add_argument(
+        "--rinfo-value", help="release info component", metavar="txt", default=""
+    )
 
     return p.parse_known_args()
 
@@ -42,26 +117,16 @@ def main():
     if unknown_args:
         log.warning("Unknown arguments: %s", unknown_args)
         sys.exit(1)
-    threading.current_thread().name = "main"
 
-    print(">> Test logging")
-    kwargs = {"extra": {"ip": "10.0.0.1"}}
-    for a in ["debug", "info", "warning", "error", "critical"]:
-        fn = getattr(log, a)
-        fn("%s msg", a)
-        fn("%s msg", a, extra=kwargs)
-    log.debug("kwargs\n%s", prettify(kwargs))
+    ver = Version()
+    cmd_vals = get_cmd_vals(args)
+    git_vals = get_vc_vals(args.path, args.ref)
+    if args.style != "all":
+        print(ver.fmt(Version.Styles[args.style], git_vals, cmd_vals))
+        return
 
-    print(">> Test terminal colors")
-    term = term_get()
-    print("try %sred%s string" % (term.red, term.normal))
-
-    print(">> Test conf")
-    print("conf: log level %s" % conf_get("level"))
-
-    print(">> Test shell")
-    sh = Shell()
-    sh("echo hello, world")
+    for k, v in Version.Styles.items():
+        print("%8s" % k, ver.fmt(v, git_vals, cmd_vals))
 
 
 if __name__ == "__main__":
